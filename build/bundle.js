7a16bd991b2d5c15ce2c71692a6bf0b6a7dbdae5
Ryan Foster <ryan.foster@salesforce.com>
2018-01-12 14:28:33 -0700

    add inline toc

--- build/bundle.js
+++ build/bundle.js
@@ -69,8 +69,37 @@
 
 __webpack_require__(1);
 
-function doSomething() {
-  return 'hello';
+buildToc(document.querySelector('.example-toc'));
+
+function buildToc(targetElement) {
+  const headings = document.querySelectorAll('h2');
+  const names = generateNames(headings);
+
+  targetElement.innerHTML = renderToc(names);
+}
+
+function generateNames(elements) {
+
+  return Array.from(elements).map((element) =>{
+    let text = element.textContent;
+    let name = text.toLowerCase().replace(' ', '-').replace(/![a-z]/g, '');
+
+    element.setAttribute('id', name);
+
+    return [text, name];
+  });
+}
+
+function renderTocItem(pair) {
+  return `<li><a title="${pair[0]}" href="#${pair[1]}">${pair[0]}</a>`;
+}
+
+function renderToc(names) {
+  const items = names.map((pair) => {
+    return renderTocItem(pair);
+  });
+
+  return `<ul>${items.join('')}</ul>`;
 }
 
 
