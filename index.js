22ad275273cf4dadfb3738c650a491be166d251b
Ryan Foster <ryan.foster@salesforce.com>
2018-02-02 12:29:32 -0700

    allow cross origin requests

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static(__dirname + '/build'));
 app.use(express.static(__dirname + '/node_modules/@salesforce-ux/design-system/assets'));
 
